ed18af9bb1b95dbe6eb815b646798a8ec1c56e40
antoniotorresz <antoniiotorresz1@gmail.com>
2024-03-28 19:47:14 -0600

    added toggle function connected with backend

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit{
+  basePath = "http://localhost:5000";
   toggleStatus: boolean = false;
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get(this.basePath + "/log").subscribe(
+      (data: any) => {
+        this.toggleStatus = data.actions[0].is_activated;
+       }
+    );
+  }
+
+  onToggleClick(event: any){
+    this.http.post(this.basePath + "/switch-action", {}).subscribe(
+      (data: any) => {
+        console.log(data);
+        this.toggleStatus = data.action.is_activated;
+      }
+    );
+  }
 
 }
